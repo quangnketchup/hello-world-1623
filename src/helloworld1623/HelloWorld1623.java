cfc96e5e342ea3d8cfbebf60de1b913cd9cb9d7b
quangnketchup <quang.nguyen20401@gmail.com>
2022-05-18 13:53:27 +0700

    1st lam chuyen ay - upload code to github

---
+++ src/helloworld1623/HelloWorld1623.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld1623;
+
+/**
+ *
+ * @author quang
+ */
+public class HelloWorld1623 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("WELCOME");
+    }
+    
+}
